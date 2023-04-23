dff089df65664daeffe33d5750d63e0b6d2e9662
roy <roy.framery@gmail.com>
2023-04-23 17:45:33 +0200

    create another component with a background image and a flatlist

--- App.jsx
+++ App.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { StyleSheet, SafeAreaView, StatusBar } from 'react-native'
 
 // components
-import CurrentWeather from './src/components/CurrentWeather'
+// import CurrentWeather from './src/components/CurrentWeather'
+import UpcommingWeather from './src/components/UpcommingWeather'
 
 const App = () => {
     return (
         <SafeAreaView style={styles.safeAreaWrapper}>
-            <CurrentWeather />
+            {/* <CurrentWeather /> */}
+            <UpcommingWeather />
         </SafeAreaView>
     )
 }
 const styles = StyleSheet.create({
     safeAreaWrapper: {
         flex: 1,
-        marginTop: StatusBar.currentHeight,
-        backgroundColor: 'pink'
+        marginTop: StatusBar.currentHeight || 0,
+        backgroundColor: 'royalblue'
     }
 })
 
---
+++ src/components/UpcommingWeather.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { View, Text, FlatList, StyleSheet, ImageBackground } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+const DATA = [
+    {
+        dt_txt: '2023-02-18 12:00:00',
+        main: {
+            temp_max: 8.55,
+            temp_min: 7.55
+        },
+        weather: [
+            {
+                main: 'Clear'
+            }
+        ]
+    },
+    {
+        dt_txt: '2023-02-18 15:00:00',
+        main: {
+            temp_max: 8.55,
+            temp_min: 7.55
+        },
+        weather: [
+            {
+                main: 'Clouds'
+            }
+        ]
+    },
+    {
+        dt_txt: '2023-02-18 18:00:00',
+        main: {
+            temp_max: 8.55,
+            temp_min: 7.55
+        },
+        weather: [
+            {
+                main: 'Rain'
+            }
+        ]
+    }
+]
+
+const Item = (props) => {
+    const { dt_txt, min, max, condition } = props
+    return (
+        <View style={styles.item}>
+            <Feather name={'sun'} size={50} color={'white'} />
+            <Text style={styles.date}>{dt_txt}</Text>
+            <Text style={styles.temp}>{min}</Text>
+            <Text style={styles.temp}>{max}</Text>
+        </View>
+    )
+}
+
+const UpcommingWeather = () => {
+    const renderItem = ({ item }) => (
+        <Item
+            condition={item.weather[0].main}
+            dt_txt={item.dt_txt}
+            min={item.main.temp_min}
+            max={item.main.temp_max}
+        />
+    )
+    return (
+        <View style={styles.upcommingwrapper}>
+            <ImageBackground
+                source={require('../../assets/upcoming-background.jpg')}
+                style={styles.image}
+            >
+                <Text>Upcoming Weather</Text>
+                <FlatList
+                    data={DATA}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.dt_txt}
+                />
+            </ImageBackground>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    upcommingwrapper: {
+        flex: 1
+    },
+    item: {
+        padding: 28,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        borderWidth: 5,
+        backgroundColor: 'pink'
+    },
+    temp: {
+        color: 'white',
+        fontSize: 20
+    },
+    date: {
+        color: 'white',
+        fontSize: 15
+    },
+    image: {
+        flex: 1
+    }
+})
+
+export default UpcommingWeather
